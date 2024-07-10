4d6dfdec92d284972eda5f1a51fcbcebe5f5001f
fefenfeng <1369508762@qq.com>
2024-07-10 15:10:36 +0800

    show loss of validation

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -67,12 +67,16 @@ class Trainer(BaseTrainer):
         pesq_c_n = []
         pesq_c_e = []
 
+        # loss total
+        loss_total = 0.0
+
         for i, (mixture, clean, name) in enumerate(self.validation_data_loader):
             assert len(name) == 1, "Only support batch size is 1 in enhancement stage."
             name = name[0]
             padded_length = 0  # 用于记录如果音频长度不是sample_length整数倍时，需要填充的长度
 
             mixture = mixture.to(self.device)  # [1, 1, T]
+            clean = clean.to(self.device)  # clean也移到device上
 
             # 模型输入是固定长度，需要将mixture文件（验证文件本来长度不限）分成许多块
             if mixture.size(-1) % sample_length != 0:
@@ -86,15 +90,23 @@ class Trainer(BaseTrainer):
             enhanced_chunks = []
             for chunk in mixture_chunks:
                 # detach()从计算图中分离tensor
-                enhanced_chunks.append(self.model(chunk).detach().cpu())
+                # enhanced_chunks.append(self.model(chunk).detach().cpu())
+                enhanced_chunks.append(self.model(chunk).detach())
             # 拼接tensor得到full tensor
             enhanced = torch.cat(enhanced_chunks, dim=-1)  # [1, 1, T]
             # 移除padded填充部分
             enhanced = enhanced if padded_length == 0 else enhanced[:, :, :-padded_length]
             mixture = mixture if padded_length == 0 else mixture[:, :, :-padded_length]
+
+            # 计算loss
+            loss = self.loss_function(clean, enhanced)
+            loss_total += loss.item()
+
             # 转成一维numpy数组
-            enhanced = enhanced.reshape(-1).numpy()
-            clean = clean.numpy().reshape(-1)
+            # enhanced = enhanced.reshape(-1).numpy()
+            # clean = clean.numpy().reshape(-1)
+            enhanced = enhanced.cpu().reshape(-1).numpy()
+            clean = clean.cpu().numpy().reshape(-1)
             mixture = mixture.cpu().numpy().reshape(-1)
 
             assert len(mixture) == len(enhanced) == len(clean)
@@ -151,6 +163,10 @@ class Trainer(BaseTrainer):
             "Clean and noisy": get_metrics_ave(pesq_c_n),
             "Clean and enhanced": get_metrics_ave(pesq_c_e)
         }, epoch)
+
+        # 记录平均损失到TensorBoard
+        dl_len = len(self.validation_data_loader)
+        self.writer.add_scalar(f"Validation/Loss", loss_total / dl_len, epoch)
 
         score = (get_metrics_ave(stoi_c_e) + self._transform_pesq_range(get_metrics_ave(pesq_c_e))) / 2
         return score
